e4447b8fed474ec88c4a3947ad379cea13d87b62
munarai <munaraii1996@gmail.com>
2023-10-21 19:14:02 -0400

    added class point

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+
+    enum TriangleType{
+        ISOCELES,
+        EQUILATERAL,
+        UNKNOWN
+    }
+    double x, y;
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceBetweenTwoPoints(Point otherCoordinate){
+        return Math.sqrt(Math.pow(x - otherCoordinate.x, 2) + Math.pow(y - otherCoordinate.y, 2));
+    }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Triangle {
+public class Triangle {
+    Point[] vertices;
+        public Triangle(Point[] vertices){
+            if(vertices.length != 3){
+                System.out.println("Must have 3 vertices.");
+            }
+            this.vertices = vertices;
+        }
 }
